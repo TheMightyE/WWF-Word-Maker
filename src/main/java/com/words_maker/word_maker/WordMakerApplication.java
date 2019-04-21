e1ca649673029dddf7850963c3ebefcffa0dd49f
TheMightyE <sk2283@njit.edu>
2019-04-21 13:38:08 -0400

    Initial commit.

---
+++ src/main/java/com/words_maker/word_maker/WordMakerApplication.java
@@ -0,0 +1,13 @@
+package com.words_maker.word_maker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WordMakerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/words_maker/word_maker/WordMakerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.words_maker.word_maker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WordMakerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
